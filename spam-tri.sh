4c4800f13d2c0b787a96737c1f874ea98f49723d
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2018-08-02 21:35:34 +0700

    Update Spam.sh

--- spam-tri.sh
+++ spam-tri.sh
@@ -1,12 +1,11 @@
 <?php
-function banner(){
-system('cls') or system('clear');
-print "[•]=====================================================[•]"
-print "|# Quotes   : Welcome to Tools-xNot_Found                #|" | lolcat
-print "|# Author   : xNot_Found                                 #|" | lolcat
-print "|# Contact  : +12674783379                               #|" | lolcat
-print "|# Thanks To: All Member TERMUX TOOLS-ID                 #|" | lolcat
-print "[•]=====================================================[•]"
+clear
+echo "[•]=====================================================[•]"
+echo "|# Quotes   : Welcome to Tools-xNot_Found                #|" | lolcat
+echo "|# Author   : xNot_Found                                 #|" | lolcat
+echo "|# Contact  : +12674783379                               #|" | lolcat
+echo "|# Thanks To: All Member TERMUX TOOLS-ID                 #|" | lolcat
+echo "[•]=====================================================[•]"
 
 if (empty($argv[1]) or empty($argv[2])) {
 banner();
